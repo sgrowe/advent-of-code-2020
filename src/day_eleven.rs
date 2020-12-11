265847caf8621787ba6cee590367f1ed6033343c
sam <sgrowe@live.co.uk>
2020-12-11 20:19:29 +0000

    Solve day eleven part two

--- src/day_eleven.rs
+++ src/day_eleven.rs
@@ -3,15 +3,21 @@ use super::utils::start_day;
 pub fn main() {
     let input = start_day("eleven");
 
-    println!("Part one: {}", part_one(&input));
-    // println!("Part two: {}", part_two(&input));
+    let seats = SeatLayout::parse(&input);
+
+    println!("Part one: {}", part_one(seats.clone()));
+    println!("Part two: {}", part_two(seats));
     println!();
 }
 
-fn part_one(input: &str) -> usize {
-    let mut seats = SeatLayout::parse(input);
+fn part_one(mut seats: SeatLayout) -> usize {
+    seats.run(false);
 
-    seats.run();
+    seats.total_occupied_seats()
+}
+
+fn part_two(mut seats: SeatLayout) -> usize {
+    seats.run(true);
 
     seats.total_occupied_seats()
 }
@@ -33,18 +39,18 @@ impl Position {
         }
     }
 
-    pub fn next(&self, num_occupied_adjacent_seat: usize) -> Self {
+    pub fn next(&self, num_occupied_adjacent_seat: usize, tolerance: usize) -> Self {
         match (self, num_occupied_adjacent_seat) {
             (Position::Empty, 0) => Position::Filled,
             (Position::Empty, _) => Position::Empty,
-            (Position::Filled, x) if x >= 4 => Position::Empty,
+            (Position::Filled, x) if x >= tolerance => Position::Empty,
             (Position::Filled, _) => Position::Filled,
             (Position::Floor, _) => Position::Floor,
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct SeatLayout {
     seats: Vec<Vec<Position>>,
 }
@@ -59,14 +65,18 @@ impl SeatLayout {
         SeatLayout { seats }
     }
 
-    pub fn run(&mut self) {
+    pub fn run(&mut self, v2: bool) {
         let row = vec![0; self.seats[0].len()];
         let mut num_occupied = vec![row; self.seats.len()];
 
         loop {
             for i in 0..self.seats.len() {
                 for j in 0..self.seats[0].len() {
-                    num_occupied[i][j] = self.occupied_adjacent_seats(i, j);
+                    num_occupied[i][j] = if v2 {
+                        self.occupied_visible_seats(i, j)
+                    } else {
+                        self.occupied_adjacent_seats(i, j)
+                    };
                 }
             }
 
@@ -74,8 +84,10 @@ impl SeatLayout {
 
             for i in 0..self.seats.len() {
                 for j in 0..self.seats[0].len() {
+                    let tolerance = if v2 { 5 } else { 4 };
+
                     let old = self.get(i, j).unwrap();
-                    let new = old.next(num_occupied[i][j]);
+                    let new = old.next(num_occupied[i][j], tolerance);
 
                     if old != new {
                         changed_count += 1;
@@ -98,8 +110,8 @@ impl SeatLayout {
     }
 
     fn occupied_adjacent_seats(&self, i: usize, j: usize) -> usize {
-        let i_min = if i > 0 { i - 1 } else { i };
-        let j_min = if j > 0 { j - 1 } else { j };
+        let i_min = i.saturating_sub(1);
+        let j_min = j.saturating_sub(1);
 
         let mut count = 0;
 
@@ -120,6 +132,60 @@ impl SeatLayout {
 
     fn get(&self, i: usize, j: usize) -> Option<Position> {
         self.seats.get(i).and_then(|row| row.get(j)).copied()
+    }
+
+    fn occupied_visible_seats(&self, i: usize, j: usize) -> usize {
+        let changes = [Change::Incr, Change::Same, Change::Decr];
+
+        let mut count = 0;
+
+        for &a in &changes {
+            for &b in &changes {
+                if a == Change::Same && b == Change::Same {
+                    continue;
+                }
+
+                if let Some(Position::Filled) = self.next_visible_seat((i, j), (a, b)) {
+                    count += 1;
+                }
+            }
+        }
+
+        count
+    }
+
+    fn next_visible_seat(
+        &self,
+        (mut x, mut y): (usize, usize),
+        (x_dir, y_dir): (Change, Change),
+    ) -> Option<Position> {
+        loop {
+            x = x_dir.step(x)?;
+            y = y_dir.step(y)?;
+
+            let pos = self.get(x, y)?;
+
+            if pos != Position::Floor {
+                return Some(pos);
+            }
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Change {
+    Incr,
+    Same,
+    Decr,
+}
+
+impl Change {
+    pub fn step(&self, x: usize) -> Option<usize> {
+        match self {
+            Change::Incr => x.checked_add(1),
+            Change::Same => Some(x),
+            Change::Decr => x.checked_sub(1),
+        }
     }
 }
 
@@ -142,6 +208,15 @@ L.LLLLL.LL
 
     #[test]
     fn sample_input_part_one() {
-        assert_eq!(part_one(&EXAMPLE.trim()), 37);
+        let seats = SeatLayout::parse(&EXAMPLE.trim());
+
+        assert_eq!(part_one(seats), 37);
+    }
+
+    #[test]
+    fn sample_input_part_two() {
+        let seats = SeatLayout::parse(&EXAMPLE.trim());
+
+        assert_eq!(part_two(seats), 26);
     }
 }
