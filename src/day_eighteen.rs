11bdfae5599e9a4ae341355961cbedeca1de8404
sam <sgrowe@live.co.uk>
2020-12-19 10:46:06 +0000

    Solve day eighteen part two

--- src/day_eighteen.rs
+++ src/day_eighteen.rs
@@ -6,10 +6,7 @@ pub fn main() {
     let input = start_day("eighteen");
 
     println!("Part one: {}", part_one(&input));
-    // println!(
-    //     "Part two: {}",
-    //     part_two(&rules, &my_ticket, &nearby_tickets)
-    // );
+    println!("Part two: {}", part_two(&input));
     println!();
 }
 
@@ -20,6 +17,17 @@ fn part_one(input: &str) -> u64 {
             let mut tokens = Tokeniser::of(l);
 
             eval_expr(&mut tokens)
+        })
+        .sum()
+}
+
+fn part_two(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|l| {
+            let mut tokens = Tokeniser::of(l);
+
+            eval_expr_v2(&mut tokens)
         })
         .sum()
 }
@@ -47,6 +55,41 @@ fn get_number(mut tokens: &mut Tokeniser) -> u64 {
     match tokens.next().unwrap() {
         Token::Number(a) => a,
         Token::OpenParen => eval_expr(&mut tokens),
+        _ => panic!(),
+    }
+}
+
+fn eval_expr_v2(mut tokens: &mut Tokeniser) -> u64 {
+    let mut value: u64 = get_number_v2(&mut tokens);
+
+    let mut multipliers = Vec::new();
+
+    while let Some(token) = tokens.next() {
+        match token {
+            Token::Add => {
+                value += get_number_v2(&mut tokens);
+            }
+            Token::Multiply => {
+                multipliers.push(value);
+                value = get_number_v2(&mut tokens);
+            }
+            Token::CloseParen => {
+                let prod: u64 = multipliers.iter().product();
+
+                return value * prod;
+            }
+            _ => panic!(),
+        }
+    }
+
+    let prod: u64 = multipliers.iter().product();
+    value * prod
+}
+
+fn get_number_v2(mut tokens: &mut Tokeniser) -> u64 {
+    match tokens.next().unwrap() {
+        Token::Number(a) => a,
+        Token::OpenParen => eval_expr_v2(&mut tokens),
         _ => panic!(),
     }
 }
@@ -123,5 +166,15 @@ mod tests {
     #[test_case("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
     fn sample_input_part_one(expr: &str, expected: u64) {
         assert_eq!(part_one(&expr), expected);
+    }
+
+    #[test_case("1 + 2 * 3 + 4 * 5 + 6", 231)]
+    #[test_case("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+    #[test_case("2 * 3 + (4 * 5)", 46)]
+    #[test_case("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445)]
+    #[test_case("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060)]
+    #[test_case("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340)]
+    fn sample_input_part_two(expr: &str, expected: u64) {
+        assert_eq!(part_two(&expr), expected);
     }
 }
